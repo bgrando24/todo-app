e16162280cd9f730a2e414afdac1efcf4dde9c58
Brandon <bgrando24@gmail.com>
2022-10-02 20:04:45 +1100

    Added all routes, RESTful API

--- server/index.js
+++ server/index.js
@@ -33,16 +33,57 @@ app.post("/todos", async (req, res) => {
     }
 });
 
+
 // get all todos -> R
+app.get('/todos', async (req, res) => {
+    try{
+
+        const allTodos = await pool.query('SELECT * FROM perntodo');
+        res.json(allTodos.rows);    //returns json array with all todos
+
+    } catch(e) {
+        console.error(e.message);
+    }
+})
 
 
 // get a single todo -> R
+app.get('/todos/:id', async(req, res) => {
+    try {
+
+        const singleTodo = await pool.query(`SELECT * FROM perntodo WHERE id = ${req.params.id}`);
+        res.json(singleTodo.rows[0]);
+
+    } catch (e) {
+        console.error(e.message);
+    }
+})
 
 
 // update a todo -> U
+app.put('/todos/:id', async(req, res) => {
+    try {
+
+        const updatedTodo = await pool.query(`UPDATE perntodo SET description = ${req.body.description}, status = ${req.body.status} WHERE id = ${req.params.id}`);
+        res.json("Todo was updated");
+
+    } catch (e) {
+        console.error(e.message);
+    }
+});
 
 
 // delete a todo -> D
+app.delete('/todos/:id', async(req, res) => {
+    try {
+
+        const deleteTodo = await pool.query(`DELETE FROM perntodo WHERE id=${req.params.id}`);
+        res.json("Todo was deleted");
+
+    } catch (e) {
+        console.error(e.message);
+    }
+});
 
 
 
